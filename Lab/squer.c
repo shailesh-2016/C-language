1105ccb0d7ab545f636fde73ccc65356880d9e50
shailesh-2016 <snmodi2016@gmail.com>
2025-03-31 11:40:31 +0530

    adddd

---
+++ Lab/squer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int multi = a * a;
+
+    printf(" ------------\n");
+    printf("|            |\n");
+    printf("| %d * %d = %d |\n", a, a, multi);
+    printf("|            |\n");
+    printf(" ------------\n");
+
+    return 0;
+}
